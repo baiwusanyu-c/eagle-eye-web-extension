4699500391fffa4de704237960829f489b433222
czh740132583 <740132583@qq.com>
2022-04-28 09:49:13 +0800

    update: 更新第三方连接

--- src/enums/link.ts
+++ src/enums/link.ts
@@ -6,7 +6,7 @@
  */
 export enum LINK {
   HOME_PAGE = 'https://eagleeye.beosin.com/',
-  GOOGLE_FEEDBACK = 'https://eagleeye.beosin.com/',
+  GOOGLE_FEEDBACK = 'https://docs.google.com/forms/d/e/1FAIpQLSdRSU58TgPAL7Xo903qJMxj3ir6QLiBjO6AaiO5xA853ZL2MQ/viewform',
 }
 
 export enum SOCIAL_LINK {
